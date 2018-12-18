279df86aaca84a178c57553f95018be219172c3b
Joaquin <cobos547@gmail.com>
2018-12-18 12:28:43 +0100

    Mas cosas

---
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Utilizamos spring con esta clase
+ * @author cobos
+ *
+ */
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		//Paso 1:-> carga el archivo de configuracion
+		ClassPathXmlApplicationContext context =
+		new ClassPathXmlApplicationContext("applicationContext.xml");
+		//Paso 2: -> obten el bean del spring container(del contexto) a partir del id que hemos definido en el contexto y la interfaz a partir de la cual la creamos
+		//Creamos un obleto a partir del la clase definida
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		//Paso 3: -> llama a los métodos del bean
+		System.out.println(theCoach.getDailyworkout());
+		//Paso 4: -> cierra contexto
+		context.close();
+
+	}
+
+}
