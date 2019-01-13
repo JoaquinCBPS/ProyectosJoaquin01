8baf349440a59e121511a38bd7a84166b63e0996
Joaquin <JoaquinCBPS@users.noreply.github.com>
2019-01-13 21:30:04 +0100

    Angularjs seccion 3

--- AngularJS/Material Ejercicio 02/Ejercicio 02/js/app.js
+++ AngularJS/Material Ejercicio 02/Ejercicio 02/js/app.js
@@ -1,11 +1,27 @@
-
 var app = angular.module('universidadApp',[ ]);
 
 app.controller('profesorCtrl',function($scope){
 
 	$scope.profesor = profesorData;
-	$scope.editando = {};  
+	$scope.editando = {};
+	$scope.EditaProfesor = function(){
 
+		angular.copy( $scope.profesor, $scope.editando); 
+
+	}
+
+	$scope.GuardarCambios = function(){
+
+		angular.copy( $scope.editando, $scope.profesor);
+	}
+	$scope.CancelarCambios = function(){
+
+		$scope.editando = {};
+	}
+
+
+
+	
 });
 
 var profesorData = {
@@ -13,4 +29,4 @@ var profesorData = {
 	bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
 	edad: 47,
 	foto: "img/juancarlos.jpg"
-}
+};
