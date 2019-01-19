e27d3149903951e6f6aeac6b3f8df365f5cd0a73
Joaquin <cobos547@gmail.com>
2019-01-19 19:45:32 +0100

    js
    js

--- AngularJS/Repeticiones/js/app.js
+++ AngularJS/Repeticiones/js/app.js
@@ -3,8 +3,27 @@
 var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
+
+	$scope.listado = ["Fernando Herrera","Melissa Flores","Juan Carlos Pineda","Top otamadre"];
 	
-		
+	$scope.listadoProfesores = { 
+		profesores: [{
+			nombre: "Fernando Herrera",
+			edad: 29,
+			clase: "PEE"
+		},
+		{
+			nombre: "Melissa Flores",
+			edad: 24,
+			clase: "ICE"
+		},
+		{
+			nombre: "Juan Carlos",
+			edad: 42,
+			clase: "Fire"
+		}
+		]
+	};	
 
 
 
