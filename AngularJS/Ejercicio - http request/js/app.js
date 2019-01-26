03437b2bf12470b99983617967ceb791342f1a4d
Joaquin <cobos547@gmail.com>
2019-01-26 12:32:23 +0100

    angular js

--- AngularJS/Ejercicio - http request/js/app.js
+++ AngularJS/Ejercicio - http request/js/app.js
@@ -6,11 +6,17 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  
+     
+     $scope.profesores = {};
+
+     $http.get('json/profesores.json').success(function(){
+     	//Codigo cueando s correcta la peticion
+     	$scope.profesores = data.profesores;
+
+     	});
 
 
   
-
 
 
 }]);
